1aff5937e22e2ac1761a3655724b789afcb1b798
kbarne12 <kbarne12@gmail.com>
2021-05-05 18:12:51 -0400

    Toys listed by birds

--- main_app/views.py
+++ main_app/views.py
@@ -69,3 +69,8 @@ class ToyUpdate(UpdateView):
 class ToyDelete(DeleteView):
   model = Toy
   success_url = '/toys/'
+
+def assoc_toy(request, finch_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
