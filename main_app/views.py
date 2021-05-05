bb27d7b01497d44ab012417956d247d005bf9ccc
kbarne12 <kbarne12@gmail.com>
2021-05-05 16:41:04 -0400

    toys associated with birds

--- main_app/views.py
+++ main_app/views.py
@@ -22,8 +22,10 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
   feeding_form = FeedingForm()
-  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form })
+  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form,
+  'toys': toys_finch_doesnt_have })
 
 def add_feeding(request, finch_id):
   form = FeedingForm(request.POST)
